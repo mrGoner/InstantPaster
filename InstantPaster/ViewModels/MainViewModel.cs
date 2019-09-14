be2fbc1d4273ff947fe46a3fc28825a7568f74e4
r.voronin <genom90@gmail.com>
2019-09-14 23:49:35 +0300

    Сохранение/загрузка конфигураций

--- InstantPaster/ViewModels/MainViewModel.cs
+++ InstantPaster/ViewModels/MainViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -11,6 +12,7 @@ using InstantPaster.Hook;
 using InstantPaster.Settings;
 using Microsoft.Expression.Interactivity.Core;
 using Clipboard = System.Windows.Clipboard;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
 
 namespace InstantPaster.ViewModels
 {
@@ -19,8 +21,8 @@ namespace InstantPaster.ViewModels
         public ObservableCollection<HotKeyViewModel> HotKeys { get; set; }
 
         public HotKeyViewModel SelectedHotKey { get; set; }
-        public bool HotKeyEditing { get; set; }
 
+        public ICommand NewConfigurationCommand { get; set; }
         public ICommand SaveConfigurationCommand { get; }
         public ICommand SaveAsConfigurationCommand { get; }
         public ICommand LoadConfigurationCommand { get; }
@@ -28,25 +30,48 @@ namespace InstantPaster.ViewModels
         public ICommand AddHotKeyCommand { get; }
         public ICommand RemoveHotKeyCommand { get; }
         public ICommand OpenDetailsCommand { get; }
+
         public ICommand StopTracking { get; }
         public ICommand StartTracking { get; }
         
         private readonly ConfigurationSerializer m_configurationSerializer;
         private readonly HookEngine m_hookEngine;
         private readonly HotKeyConfigurationFactory m_factory;
+        private bool m_isDocumentLoaded;
+
+
+        public bool IsDocumentLoaded
+        {
+            get => m_isDocumentLoaded;
+
+            private set
+            {
+                if (m_isDocumentLoaded != value)
+                {
+                    m_isDocumentLoaded = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string m_loadedDocumentPath;
 
         public MainViewModel()
         {
             HotKeys = new ObservableCollection<HotKeyViewModel>();
             m_configurationSerializer = new ConfigurationSerializer();
             m_hookEngine = new HookEngine();
+
             m_factory = new HotKeyConfigurationFactory(new Dictionary<ActionType, Action<string>>
             {
-                { ActionType.InsertText, PastText}
+                {ActionType.InsertText, PastText},
+                {ActionType.ExecutePath, Execute}
             });
 
+            NewConfigurationCommand = new ActionCommand(ClearCurrentConfiguration);
             LoadConfigurationCommand = new ActionCommand(LoadConfiguration);
-            SaveConfigurationCommand = new ActionCommand(SaveConfiguration);
+            SaveConfigurationCommand = new ActionCommand(() => SaveConfiguration(m_loadedDocumentPath));
+            SaveAsConfigurationCommand = new ActionCommand(OpenSaveDialog);
 
             AddHotKeyCommand = new ActionCommand(() =>
             {
@@ -62,22 +87,17 @@ namespace InstantPaster.ViewModels
                 HotKeys.Remove(SelectedHotKey);
                 SelectedHotKey = null;
 
-                m_hookEngine.SetHotKeys(GenerateConfigurations());
-                m_hookEngine.StartTracking();
+                HotKeyViewModelChanged();
             });
 
             StopTracking = new ActionCommand(() => m_hookEngine.StopTracking());
             StartTracking = new ActionCommand(() => m_hookEngine.StartTracking());
         }
 
-        private List<HotKeyConfiguration> GenerateConfigurations()
-        {
-            return HotKeys.Select(_x => m_factory.Create(_x.HotKey, _x.PastedText, ActionType.InsertText)).ToList();
-        }
-
         private void HotKeyViewModelChanged()
         {
-            m_hookEngine.SetHotKeys(GenerateConfigurations());
+            var keys = HotKeys.Select(_x => m_factory.Create(_x.HotKey, _x.PastedText, ActionType.InsertText)).ToList();
+            m_hookEngine.SetHotKeys(keys);
             m_hookEngine.StartTracking();
         }
 
@@ -85,33 +105,62 @@ namespace InstantPaster.ViewModels
         {
             try
             {
-                var data = File.ReadAllText("test.json");
-                var settings = m_configurationSerializer.Deserialize(data).HotKeys;
-                var hotKeyConfigurations = settings.Select(_hotKey => m_factory.Create(_hotKey)).ToList();
-
-                m_hookEngine.SetHotKeys(hotKeyConfigurations);
-
-                var viewModels = settings.Select(_x =>
-                    new HotKeyViewModel(_x.Combination, _x.Description, _x.ActionContent)).ToList();
-
-                foreach (var hotKeyViewModel in viewModels)
+                var folderBrowser = new OpenFileDialog
                 {
-                    hotKeyViewModel.CombinationChanged += HotKeyViewModelChanged;
+                    CheckFileExists = true,
+                    Filter = "HotKeysConfiguration|*.hkconf;",
+                    Multiselect = false,
+                    InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+                };
+
+                var openResult = folderBrowser.ShowDialog();
+
+                if (openResult.HasValue && openResult.Value)
+                {
+                    var data = File.ReadAllText(folderBrowser.FileName);
+                    var settings = m_configurationSerializer.Deserialize(data).HotKeys;
+                    var hotKeyConfigurations = settings.Select(_hotKey => m_factory.Create(_hotKey)).ToList();
+
+                    m_hookEngine.SetHotKeys(hotKeyConfigurations);
+
+                    var viewModels = settings.Select(_x =>
+                        new HotKeyViewModel(_x.Combination, _x.Description, _x.ActionContent)).ToList();
+
+                    foreach (var hotKeyViewModel in viewModels)
+                    {
+                        hotKeyViewModel.CombinationChanged += HotKeyViewModelChanged;
+                    }
+
+                    HotKeys = new ObservableCollection<HotKeyViewModel>(viewModels);
+
+                    m_hookEngine.StartTracking();
+
+                    IsDocumentLoaded = true;
+                    m_loadedDocumentPath = folderBrowser.FileName;
+
+                    OnPropertyChanged(nameof(HotKeys));
                 }
-
-                HotKeys = new ObservableCollection<HotKeyViewModel>(viewModels);
-
-                m_hookEngine.StartTracking();
-                OnPropertyChanged(nameof(HotKeys));
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
-                Console.WriteLine(e);
-                throw;
+                MessageBox.Show("Произошла ошибка чтения из файла", "Ошибка", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
             }
         }
 
-        private void SaveConfiguration()
+        private void ClearCurrentConfiguration()
+        {
+            m_hookEngine.StopTracking();
+
+            foreach (var hotkey in HotKeys)
+            {
+                hotkey.CombinationChanged -= HotKeyViewModelChanged;
+            }
+            
+            HotKeys.Clear();
+        }
+
+        private void SaveConfiguration(string _savePath)
         {
             try
             {
@@ -119,12 +168,33 @@ namespace InstantPaster.ViewModels
                     new HotKeySettings(_hotKey.HotKey, _hotKey.Description, ActionType.InsertText,
                         _hotKey.PastedText)).ToList()));
 
-                File.WriteAllText("test.json", result);
+                File.WriteAllText(_savePath, result);
+
+                m_loadedDocumentPath = _savePath;
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
-                Console.WriteLine(e);
-                throw;
+                MessageBox.Show("Произошла ошибка при сохранении в файл", "Ошибка", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void OpenSaveDialog()
+        {
+            var fileBrowser = new SaveFileDialog
+            {
+                CheckPathExists = true,
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                DefaultExt = "hkconf",
+                AddExtension = true,
+                Filter = "HotKeysConfiguration|*.hkconf;"
+            };
+
+            var saveResult = fileBrowser.ShowDialog();
+
+            if (saveResult == DialogResult.OK)
+            {
+                SaveConfiguration(fileBrowser.FileName);
             }
         }
 
@@ -137,6 +207,13 @@ namespace InstantPaster.ViewModels
             Console.WriteLine($"Sended {_content}");
         }
 
+        private void Execute(string _content)
+        {
+            Process.Start(_content);
+
+            Console.WriteLine("Executed");
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected virtual void OnPropertyChanged([CallerMemberName] string _propertyName = null)
@@ -145,3 +222,4 @@ namespace InstantPaster.ViewModels
         }
     }
 }
+
